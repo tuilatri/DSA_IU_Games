107f893528027703598c2034ec15fbac3cfe224e
Hà Minh Trí <trihaminh2004@gmail.com>
2024-12-12 01:07:31 +0700

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 package src;
 
+// khi nào xong thì hẳn xóa comment, do save lại thì vscode lại cho mất
+import javax.swing.JFrame;
+
 public class App {
-    
+
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21;                                  // tổng số dòng
+        int columnCount = 19;                               // tổng số cột
+        int tileSize = 32;                                  // kích cỡ của 1 ô
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("DSA - IU - PacMan");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
