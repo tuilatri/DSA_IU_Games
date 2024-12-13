003afd87ed56bc69f77ff580d280ef65ff6dc1da
Hà Minh Trí <trihaminh2004@gmail.com>
2024-12-13 14:04:33 +0700

    initial

--- src/App.java
+++ src/App.java
@@ -1,22 +1,27 @@
 package src;
 
-// khi nào xong thì hẳn xóa comment, do save lại thì vscode lại cho mất
 import javax.swing.JFrame;
 
 public class App {
-
     public static void main(String[] args) throws Exception {
-        int rowCount = 21;                                  // tổng số dòng
-        int columnCount = 19;                               // tổng số cột
-        int tileSize = 32;                                  // kích cỡ của 1 ô
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
         int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
 
-        JFrame frame = new JFrame("DSA - IU - PacMan"); // hiện tên lên khung hình
-        frame.setVisible(true);
+        JFrame frame = new JFrame("PacMan - DSA - IU");
+        // frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
+
     }
 }
